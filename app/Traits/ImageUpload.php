02f15e8427eea723ea9d5d8110dd0870a0791400
Ammad Zafar <zammad658@gmail.com>
2020-11-04 11:26:24 +0500

    store and delete image on cludinary

--- app/Traits/ImageUpload.php
+++ app/Traits/ImageUpload.php
@@ -20,7 +20,7 @@ trait ImageUpload {
             }else{
                 Cloudder::upload($image);
             }
-            dd(Cloudder::getResult());
+//            dd(Cloudder::getResult());
 
             $imageUrl = Cloudder::getResult()['url'];
             $publicId = Cloudder::getResult()['public_id'];
